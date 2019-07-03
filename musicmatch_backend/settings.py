55201bb07c1e471483877a287a2d47abc8f5dc4e
Jackson Gomes <jgomes@ceulp.edu.br>
2019-07-03 14:46:23 -0300

    feat: define configurações de paginação, corrige arquivo gitignore e adiciona arquivo versionrc

--- musicmatch_backend/settings.py
+++ musicmatch_backend/settings.py
@@ -132,8 +132,9 @@ REST_FRAMEWORK = {
     # 'DEFAULT_RENDERER_CLASSES': (
     #     'rest_framework.renderers.JSONRenderer',
     # ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 10,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
 }
 
